81a759a0bf60c0369c09902e6c2e507fa5b0b013
Dmitrijs Braslis <test@test.lv>
2021-04-15 20:36:21 +0300

    Initial commit

---
+++ src/main/java/FirstJavaCode.java
@@ -0,0 +1,35 @@
+import java.math.BigDecimal;
+
+public class FirstJavaCode {
+
+    public static void main(String[] args) {
+        //This code will print text in console
+        System.out.println("-----------------------------------");
+        System.out.println("Hello, corona!");
+        System.out.println("-----------------------------------");
+
+        Integer firstInt = 18;
+        Integer secondInt = 10;
+
+        String firstString = "18";
+        String secondString = "10";
+
+
+        System.out.println(firstInt + secondInt);
+        System.out.println(firstString + secondString);
+
+        String firstName = "Vasja";
+        String lastName = "Petichkin";
+
+        System.out.println(firstName + lastName);
+        System.out.println(firstName + " " + lastName);
+
+        /*
+        This
+        is
+        multiple
+        line
+        comment
+         */
+    }
+}
